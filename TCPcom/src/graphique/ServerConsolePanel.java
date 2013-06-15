65b0f3d25b8759156bc8ae4f3a9bde034b2c8431
Ulrich <ulrich@ulrich-System-Product-Name.(none)>
2013-06-15 10:55:21 +0200

    GUI & ItemCard changed conflits fixed & new files added

---
+++ TCPcom/src/graphique/ServerConsolePanel.java
@@ -0,0 +1,45 @@
+package graphique;
+
+/*package graphique;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.border.TitledBorder;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyleContext;
+import javax.swing.text.Style;
+import javax.swing.text.StyledDocument;
+import java.net.Inet6Address; 
+
+import java.net.InetAddress; 
+
+import java.util.Enumeration; 
+
+import java.net.NetworkInterface; 
+import java.net.SocketException; 
+import java.util.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+public class ServerConsolePanel 
+{
+	
+}*/
---
+++ TCPcom/src/graphique/serverAutomatePanel.java
@@ -0,0 +1,6 @@
+package graphique;
+
+public class serverAutomatePanel 
+{
+
+}
