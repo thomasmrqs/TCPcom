9030828c6bb656b7768d79aceca90fa01d82e91a
baptmobil <baptiste.vidart@gmail.com>
2013-06-15 23:44:07 +0200

    modification de la methode d'affichage de l'etat courant

--- TCPcom/src/connexion/Automate.java
+++ TCPcom/src/connexion/Automate.java
@@ -134,40 +134,43 @@ public class Automate implements Runnable {
         }
     }
 
-    public void afficheEtatCourant() {
-        switch (this.etatCourant) {
+    public void afficheEtatCourant()
+    {
+    	String state = ((this.getMod()) ? "client" : "serveur" );
+        switch (this.etatCourant)
+        {
             case Ressource.ETAT_CLOSE_WAIT:
-                System.out.println("Etat actuel : ETAT_CLOSE_WAIT");
+                System.out.println("Etat " + state + " actuel : ETAT_CLOSE_WAIT");
                 break;
             case Ressource.ETAT_CLOSED:
-                System.out.println("Etat actuel : ETAT_CLOSED");
+                System.out.println("Etat " + state + " actuel : ETAT_CLOSED");
                 break;
             case Ressource.ETAT_CLOSING:
-                System.out.println("Etat actuel : ETAT_CLOSING");
+                System.out.println("Etat " + state + " actuel : ETAT_CLOSING");
                 break;
             case Ressource.ETAT_ESTABLISHED:
-                System.out.println("Etat actuel : ETAT_ESTABLISHED");
+                System.out.println("Etat " + state + " actuel : ETAT_ESTABLISHED");
                 break;
             case Ressource.ETAT_FIN_WAIT_1:
-                System.out.println("Etat actuel : ETAT_FIN_WAIT_1");
+                System.out.println("Etat " + state + " actuel : ETAT_FIN_WAIT_1");
                 break;
             case Ressource.ETAT_FIN_WAIT_2:
-                System.out.println("Etat actuel : ETAT_FIN_WAIT_2");
+                System.out.println("Etat " + state + " actuel : ETAT_FIN_WAIT_2");
                 break;
             case Ressource.ETAT_LAST_ACK:
-                System.out.println("Etat actuel : ETAT_LAST_ACK");
+                System.out.println("Etat " + state + " actuel : ETAT_LAST_ACK");
                 break;
             case Ressource.ETAT_LISTEN:
-                System.out.println("Etat actuel : ETAT_LISTEN");
+                System.out.println("Etat " + state + " actuel : ETAT_LISTEN");
                 break;
             case Ressource.ETAT_SYN_RCVD:
-                System.out.println("Etat actuel : ETAT_SYN_RCVD");
+                System.out.println("Etat " + state + " actuel : ETAT_SYN_RCVD");
                 break;
             case Ressource.ETAT_SYN_SENT:
-                System.out.println("Etat actuel : ETAT_SYN_SENT");
+                System.out.println("Etat " + state + " actuel : ETAT_SYN_SENT");
                 break;
             case Ressource.ETAT_TIME_WAIT:
-                System.out.println("Etat actuel : ETAT_TIME_WAIT");
+                System.out.println("Etat " + state + " actuel : ETAT_TIME_WAIT");
                 break;
             default:
                 System.out.println("Etat inconnu");
